ba16fb7a65e08206cd36dade81af837aa45651c5
Oladotun Adedeji <47560696+d8zzy@users.noreply.github.com>
2023-03-24 00:47:04 +0100

    final push

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('header') header: any;
+  menu: boolean = false;
+  prevScrollpos: number = window.pageYOffset;
+  currentScrollPos: number = 0;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  @HostListener('window:scroll', ['$event']) scrollEl(ev: any) {
+    this.currentScrollPos = ev.target.scrollingElement.scrollTop;
+    this.prevScrollpos > this.currentScrollPos
+      ? this.renderer.setStyle(
+          this.header.nativeElement,
+          'transform',
+          'translateY(0%) translateY(-17px)'
+        )
+      : this.renderer.setStyle(
+          this.header.nativeElement,
+          'transform',
+          'translateY(-100%) translateY(-100px)'
+        );
+    this.prevScrollpos = this.currentScrollPos;
   }
-
+  toggleMenu() {
+    this.menu = !this.menu;
+  }
+  close() {
+    this.menu = !this.menu;
+  }
 }
